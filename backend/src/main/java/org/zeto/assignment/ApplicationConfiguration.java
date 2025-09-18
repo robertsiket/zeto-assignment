41f3aaca5876a825f58c3b52e72885a4472e16ad
robert.siket <robert.siket@edetek.com>
2025-09-18 14:23:14 +0200

    Initialize project structure with Spring Boot backend and Angular frontend. Add IntelliJ IDEA configuration files and update `.gitignore`.

---
+++ backend/src/main/java/org/zeto/assignment/ApplicationConfiguration.java
@@ -0,0 +1,13 @@
+package org.zeto.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ApplicationConfiguration {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationConfiguration.class, args);
+    }
+}
